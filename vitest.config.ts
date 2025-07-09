caf4dac884ea1db1049b007c06080eb8268dcef0
Justin Mak <jp.mak44@gmail.com>
2025-07-09 20:13:28 +0200

    fixed more import errors

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
     test: {
         environment: 'jsdom',
-        globals: true,       // if you prefer `describe/it` globally
+        globals: true,
+        coverage: {
+            enabled: true, // 👈 Ensures coverage is ALWAYS on for UI and CLI
+            reporter: ['text', 'html'], // 👈 HTML for UI, text for CLI output
+            reportsDirectory: './coverage', // (optional) where html report goes
+            provider: 'v8' // or 'v8'
+        }
+        // Vitest will use the same `resolve.alias` as Vite automatically,
+        // so you don't need to repeat it here.
     },
 })
