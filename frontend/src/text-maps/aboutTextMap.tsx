ba65ad44f58052c04b34fa8ac0b7829bf9cefb82
Justin Mak <jp.mak44@gmail.com>
2025-08-25 18:47:33 +0200

    fixed links

--- frontend/src/text-maps/aboutTextMap.tsx
+++ frontend/src/text-maps/aboutTextMap.tsx
@@ -5,11 +5,23 @@ export const aboutTextMap = {
       {
         text: "Sales",
         className: "sales-content",
-        link: "https://airflow.apache.org/",
+        link: "",
       },
-      { text: "Support", className: "support-content", link: "" },
-      { text: "Consulting", className: "consulting-content", link: "" },
-      { text: "Recruiting", className: "recruiting-content", link: "" },
+      {
+        text: "Support",
+        className: "support-content",
+        link: "https://outlook.office.com/mail/deeplink/compose?",
+      },
+      {
+        text: "Consulting",
+        className: "consulting-content",
+        link: "https://outlook.office.com/mail/deeplink/compose?",
+      },
+      {
+        text: "Recruiting",
+        className: "recruiting-content",
+        link: "https://www.linkedin.com/company/olevius/jobs/",
+      },
     ],
     classNameTitle: "use-cases-title",
   },
@@ -41,8 +53,16 @@ export const aboutTextMap = {
     content: [
       { text: "Pricing", className: "resources-pricing", link: "" },
       { text: "Manifesto", className: "resources-manifesto", link: "" },
-      { text: "Press", className: "resources-press", link: "" },
-      { text: "Careers", className: "resources-careers", link: "" },
+      {
+        text: "Press",
+        className: "resources-press",
+        link: "https://www.linkedin.com/company/olevius/posts/?feedView=all",
+      },
+      {
+        text: "Careers",
+        className: "resources-careers",
+        link: "https://www.linkedin.com/company/olevius/jobs/",
+      },
     ],
     classNameTitle: "resources-title",
   },
@@ -50,8 +70,26 @@ export const aboutTextMap = {
   support: {
     title: "Support",
     content: [
-      { text: "Help Center", className: "support-help-center", link: "" },
-      { text: "Contact Us", className: "support-contact-us", link: "" },
+      {
+        text: "Help Center",
+        className: "support-help-center",
+        link:
+          "https://outlook.office.com/mail/deeplink/compose?" +
+          new URLSearchParams({
+            to: "j29mak@uwaterloo.ca",
+            subject: "Olevius_inquiry",
+          }).toString(), // Outlook handles + as spaces fine,
+      },
+      {
+        text: "Contact Us",
+        className: "support-contact-us",
+        link:
+          "https://outlook.office.com/mail/deeplink/compose?" +
+          new URLSearchParams({
+            to: "j29mak@uwaterloo.ca",
+            subject: "Olevius_inquiry",
+          }).toString(), // Outlook handles + as spaces fine,
+      },
     ],
     classNameTitle: "support-title",
   },
@@ -59,9 +97,21 @@ export const aboutTextMap = {
   legal: {
     title: "Legal",
     content: [
-      { text: "Privacy Policy", className: "legal-privacy-policy", link: "" },
-      { text: "Terms of Service", className: "legal-terms", link: "" },
-      { text: "Data Processing Agreement", className: "legal-dpa", link: "" },
+      {
+        text: "Privacy Policy",
+        className: "legal-privacy-policy",
+        link: "https://opensource.org/license/bsd-3-clause",
+      },
+      {
+        text: "Terms of Service",
+        className: "legal-terms",
+        link: "https://opensource.org/license/bsd-3-clause",
+      },
+      {
+        text: "Data Processing Agreement",
+        className: "legal-dpa",
+        link: "https://uwaterloo.ca/nanotechnology-engineering/undergraduate-students/capstone-design",
+      },
     ],
     classNameTitle: "legal-title",
   },
