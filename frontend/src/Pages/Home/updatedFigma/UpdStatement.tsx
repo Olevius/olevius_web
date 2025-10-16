b94c1fb11f4e329cb4cf60d1c590dd7172c69fd6
jpcodes44 <jp.mak44@gmail.com>
2025-10-16 09:17:30 -0400

    built the statement page from figma

--- frontend/src/Pages/Home/updatedFigma/UpdStatement.tsx
+++ frontend/src/Pages/Home/updatedFigma/UpdStatement.tsx
@@ -1,4 +1,4 @@
-import { Layout } from "../../../components/basics/defaults";
+import { Layout, Text } from "../../../components/basics/defaults";
 import type { BaseProps } from "../../../components/basics/defaultTypes";
 import { forRef } from "../../../components/basics/refHelper";
 
@@ -8,7 +8,17 @@ export const UpdStatement = forRef<HTMLDivElement, Omit<BaseProps, "children">>(
       className={className}
       ref={ref}
       {...rest}
-      style={{ ...style }}
-    ></Layout>
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white",
+        width: "100vw",
+        height: "100vh",
+        ...style,
+      }}
+    >
+      <Text className="upd-statement-text">We're changing that.</Text>
+    </Layout>
   )
 );
