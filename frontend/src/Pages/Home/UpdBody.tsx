3b36cfcc177ddfb63e8f9520a80813375859976a
jpcodes44 <jp.mak44@gmail.com>
2025-10-16 00:28:11 -0400

    added empty functions for each newly added figma component

---
+++ frontend/src/Pages/Home/UpdBody.tsx
@@ -0,0 +1,14 @@
+import { Layout } from "../../components/basics/defaults";
+import type { BaseProps } from "../../components/basics/defaultTypes";
+import { forRef } from "../../components/basics/refHelper";
+
+export const UpdBody = forRef<HTMLDivElement, Omit<BaseProps, "children">>(
+  ({ style, className, ...rest }, ref) => (
+    <Layout
+      className={className}
+      ref={ref}
+      {...rest}
+      style={{ ...style }}
+    ></Layout>
+  )
+);
