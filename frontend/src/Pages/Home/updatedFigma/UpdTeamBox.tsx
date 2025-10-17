f33cd722e0acceb217f025debc3de6401feb502e
jpcodes44 <jp.mak44@gmail.com>
2025-10-17 17:34:48 -0400

    built the team page from figma without css mediaqueries yet

--- frontend/src/Pages/Home/updatedFigma/UpdTeamBox.tsx
+++ frontend/src/Pages/Home/updatedFigma/UpdTeamBox.tsx
@@ -1,14 +1,54 @@
-import { Layout } from "../../../components/basics/defaults";
-import type { BaseProps } from "../../../components/basics/defaultTypes";
+import { Layout, Text } from "../../../components/basics/defaults";
+import type { TeamBoxProps } from "../../../components/basics/defaultTypes";
 import { forRef } from "../../../components/basics/refHelper";
+import { UpdLine } from "./UpdLine";
 
-export const UpdTeamBox = forRef<HTMLDivElement, Omit<BaseProps, "children">>(
-  ({ style, className, ...rest }, ref) => (
+export const UpdTeamBox = forRef<HTMLDivElement, TeamBoxProps>(
+  ({ style, children, bottomLine, className, ...rest }, ref) => (
     <Layout
       className={className}
       ref={ref}
       {...rest}
-      style={{ ...style }}
-    ></Layout>
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "3vh",
+        ...style,
+      }}
+    >
+      <UpdLine />
+
+      <Layout
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          flexDirection: "row",
+          paddingLeft: "10rem",
+          paddingRight: "10rem",
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "satoshi",
+            fontWeight: "lighter",
+            fontSize: "3.125rem",
+          }}
+        >
+          {children}
+        </Text>
+        <Text
+          style={{
+            fontFamily: "satoshi",
+            fontWeight: "lighter",
+            fontSize: "3.125rem",
+          }}
+        >
+          +
+        </Text>
+      </Layout>
+      <UpdLine
+        className={bottomLine ? undefined : "upd-team-box-disable-bottom-line"}
+      />
+    </Layout>
   )
 );
